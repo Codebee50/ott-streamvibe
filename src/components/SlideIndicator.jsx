ffdacce58c3eb9dfe920e7bd9ff2743bdeca4754
Code bee <codebee345@outlook.com>
2024-04-26 08:45:02 +0100

    added desktop scrolling functionality

--- src/components/SlideIndicator.jsx
+++ src/components/SlideIndicator.jsx
@@ -1,24 +1,52 @@
 import { HiArrowRight } from "react-icons/hi";
 import { HiArrowLeft } from "react-icons/hi";
 
-const SlideIndicator = () => {
+const SlideIndicator = (props) => {
+  const trSlideRatio = Math.ceil(props.slideRatio);
+  const remainder = props.slideRatio % Math.floor(props.slideRatio) === 0;
+
   return (
     <div className="bg-black06  px-2  py-2 rounded-[0.9rem] border-[2px] border-black10 flex flex-row gap-2">
-      <div className="bg-black10 p-[10px] rounded-md cursor-pointer">
+      <div
+        className="bg-black10 p-[10px] rounded-md cursor-pointer"
+        onClick={props.onLeftClick}
+      >
         <HiArrowLeft fill="#ffffff" />
       </div>
 
       <div className="flex flex-row items-center gap-[3px]">
-        <span className="w-[23px]  h-[4px] bg-red45 rounded-xl"></span>
+        {props.slideRatio &&
+          Array(trSlideRatio)
+            .fill()
+            .map((_, index) => {
+              return (
+                <span
+                  key={index}
+                  className={`w-4 h-[4px] bg-black20 rounded-xl ${
+                    index === props.scrollIndex && "bg-red45"
+                  }`}
+                ></span>
+              );
+            })}
+
+        {!remainder && (
+          <span
+            className={`w-2 h-[4px] bg-black20 rounded-xl ${
+              props.slideRatio - props.scrollIndex < 1 && "bg-red45"
+            }`}
+          ></span>
+        )}
+        {/* <span className="w-[23px]  h-[4px] bg-red45 rounded-xl"></span>
         <span className="w-4 h-[4px] bg-black20 rounded-xl"></span>
         <span className="w-4 h-[4px] bg-black20 rounded-xl"></span>
-        <span className="w-4 h-[4px] bg-black20 rounded-xl"></span>
+        <span className="w-4 h-[4px] bg-black20 rounded-xl"></span> */}
       </div>
 
-
-      <div className="bg-black10 p-[10px] rounded-md cursor-pointer">
+      <div
+        className="bg-black10 p-[10px] rounded-md cursor-pointer"
+        onClick={props.onRightClick}
+      >
         <HiArrowRight fill="#ffffff" />
-
       </div>
     </div>
   );
--- src/sections/Categories.jsx
+++ src/sections/Categories.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 import MovieCollectionCard from "../components/MovieCollectionCard";
 import SlideIndicator from "../components/SlideIndicator";
 import TextL from "../components/TextL";
@@ -6,8 +6,13 @@ import TextSm from "../components/TextSm";
 
 const Categories = () => {
   const [genreList, setGenreList] = useState([]);
-  const [transformedGenreList, setTransformedGenreList] = useState([]);
   const [moviesList, setMoviesList] = useState([]);
+  const categoriesContainerRef = useRef();
+  const [scrollIndex, setScrollIndex] = useState(0);
+  const [scrollLeft, setSCrollLeft] = useState(0);
+  const [nextPerfectClip, setNextPerfectLip] = useState(0);
+
+  let lastPerfect = 0;
 
   function getjson(url, errorMsg = "Something went wrong") {
     return fetch(url).then((response) => {
@@ -44,45 +49,13 @@ const Categories = () => {
         });
       });
       setMoviesList(movies);
+      // setSlideRatio(getSlideRatio());
     }
   }, [genreList]);
 
   useEffect(() => {
     getMoviesList();
   }, [getMoviesList]);
-
-  async function getImageLinks(genreId, genreName) {
-    let foundNum = 0;
-    let imageLInks = [];
-
-    if (foundNum < 4) {
-      const responose = await fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${
-          import.meta.env.VITE_TMDP_API_TOKEN
-        }&with_genres=${genreId}`
-      );
-
-      if (responose.ok) {
-        const movieGenre = await responose.json();
-        movieGenre.results.forEach((movie) => {
-          if (foundNum >= 4) {
-            return;
-          }
-
-          if (movie.genre_ids.includes(genreId)) {
-            imageLInks.push(movie.poster_path);
-            foundNum += 1;
-          }
-        });
-      }
-    }
-
-    return {
-      id: genreId,
-      name: genreName,
-      imageLinks: imageLInks,
-    };
-  }
 
   function fechPosterImagesByGenreId(genreId) {
     const foundPosters = [];
@@ -115,20 +88,52 @@ const Categories = () => {
       });
   }
 
-  const transformGenreList = useCallback(async () => {
-    try {
-      const genreMovies = await Promise.all(
-        genreList.map((genre) => getImageLinks(genre.id, genre.name))
-      );
-      setTransformedGenreList(genreMovies);
-    } catch (err) {
-      console.error(err);
-    }
-  }, [genreList]);
+  function leftSlideHandler() {
+    categoriesContainerRef.current.scrollLeft -=
+      categoriesContainerRef.current.getBoundingClientRect().width;
 
+    // if (scrollIndex - 1 >= 0) {
+    //   setScrollIndex((prevIndex) => prevIndex - 1);
+    // }
+  }
+
+  function rightSlideHandler() {
+    categoriesContainerRef.current.scrollLeft +=
+      categoriesContainerRef.current.getBoundingClientRect().width;
+
+    // if (getSlideRatio() - scrollIndex > 0 || scrollIndex === 0) {
+    //   setScrollIndex((prevIndex) => prevIndex + 1);
+    // }
+  }
+
+  // num of items * width of individual item/ window.innerWidth
   useEffect(() => {
     fetchGenres();
   }, []);
+
+  useEffect(() => {
+    const scrollLeftDebouncer = setTimeout(() => {
+      const containerWidth =
+        categoriesContainerRef.current.getBoundingClientRect().width;
+
+      setScrollIndex(Math.floor(scrollLeft / containerWidth));
+    }, 200);
+
+    return () => {
+      clearTimeout(scrollLeftDebouncer);
+    };
+  }, [scrollLeft]);
+
+  function slideScrollHandler(event) {
+    setSCrollLeft(event.target.scrollLeft);
+  }
+
+  function getSlideRatio() {
+    const slideRatio =
+      (genreList.length * 236) /
+      categoriesContainerRef?.current?.getBoundingClientRect().width;
+    return slideRatio || null;
+  }
 
   return (
     <section className="w-full min-h-[2vh] bg-page-black padding-x max-container flex flex-col pt-[35vh] pb-[20vh]">
@@ -139,11 +144,20 @@ const Categories = () => {
         </div>
 
         <div>
-          <SlideIndicator />
+          <SlideIndicator
+            onRightClick={rightSlideHandler}
+            onLeftClick={leftSlideHandler}
+            slideRatio={getSlideRatio()}
+            scrollIndex={scrollIndex}
+          />
         </div>
       </div>
 
-      <div className="mt-10 w-full flex flex-row overflow-x-scroll gap-3 no-scrollbar">
+      <div
+        className="mt-10 w-full flex flex-row overflow-x-scroll gap-3 no-scrollbar"
+        ref={categoriesContainerRef}
+        onScroll={slideScrollHandler}
+      >
         {genreList.map((genreItem) => {
           const posterPaths = fechPosterImagesByGenreId(genreItem.id);
           return (
